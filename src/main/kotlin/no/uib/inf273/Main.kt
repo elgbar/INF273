1e3ca9a01d34625e0d8f9b20479783e5628bb03b
Elg <klink100@gmail.com>
2020-02-12 13:54:02 +0100

    Implement benchmark for assignment 3

--- src/main/kotlin/no/uib/inf273/Main.kt
+++ src/main/kotlin/no/uib/inf273/Main.kt
@@ -11,6 +11,7 @@ import no.uib.inf273.search.RandomSearch
 import no.uib.inf273.search.Search
 import no.uib.inf273.search.SimulatedAnnealingSearch
 import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 
 class Main(
@@ -48,7 +49,6 @@ class Main(
 
     val benchmark: Boolean by parser.flagging(
         "--benchmark-assignment-3",
-        "-b",
         help = "Enable benchmarking as specified in Assignment 3. Search option will be ignored."
     ).default(false)
 
@@ -70,7 +70,14 @@ class Main(
         solgen = SolutionGenerator(data)
 
         if (benchmark) {
-            benchmarkA3()
+            val result = benchmarkA3()
+            log { "Results for instance $file" }
+            val defaultObjVal = solgen.generateStandardSolution().objectiveValue(false)
+            for ((alg, triple) in result) {
+                val (avg, best, ms) = triple
+                val improvement = 100 * (defaultObjVal - best) / defaultObjVal
+                log { "${alg.javaClass.simpleName} | $avg | $best | $improvement (%) | $ms ms" }
+            }
         } else {
             require(search != Search.NoSearch) { "Search method must be specified when no other option is selected." }
 
@@ -90,8 +97,38 @@ class Main(
         }
     }
 
-    fun benchmarkA3() {
+    /**
+     * Benchmark Local search, random search and simulated annealing as specified in assignment 3.
+     *
+     * Note that this is only done for the specified instance and not for all five.
+     *
+     * @return A map of the search mapping to average obj value, best obj val, then running time in ms
+     */
+    fun benchmarkA3(): Map<Search, Triple<Double, Int, Long>> {
 
+        val map: MutableMap<Search, Triple<Double, Int, Long>> = HashMap()
+
+        val repeats = 10
+
+        for (search in listOf(RandomSearch, LocalSearchA3, SimulatedAnnealingSearch)) {
+            log { "Running algorithm ${search.javaClass.simpleName}" }
+            var totalObj = 0.0
+            var bestObj = Int.MAX_VALUE
+
+            val time = measureTimeMillis {
+                for (i in 1..repeats) {
+                    val sol = search.search(solgen.generateStandardSolution())
+                    val objVal = sol.objectiveValue(modified = false)
+                    totalObj += objVal
+                    if (objVal < bestObj) {
+                        bestObj = objVal
+                    }
+                }
+            }
+            map[search] = Triple(totalObj / repeats, bestObj, time)
+        }
+
+        return map
     }
 
     companion object {
--- src/main/kotlin/no/uib/inf273/search/SimulatedAnnealingSearch.kt
+++ src/main/kotlin/no/uib/inf273/search/SimulatedAnnealingSearch.kt
@@ -4,6 +4,7 @@ import no.uib.inf273.processor.Solution
 
 object SimulatedAnnealingSearch : Search {
     override fun search(sol: Solution, iterations: Int): Solution {
-        TODO("not implemented")
+//        TODO("not implemented")
+        return sol
     }
 }
