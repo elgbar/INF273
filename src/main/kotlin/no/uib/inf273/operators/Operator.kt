bc2584777ddb17aeb5e65fbab66bf81535a7343e
Elg <klink100@gmail.com>
2020-03-02 15:17:15 +0100

    Extract moving a cargo from one vessel to another

--- src/main/kotlin/no/uib/inf273/operators/Operator.kt
+++ src/main/kotlin/no/uib/inf273/operators/Operator.kt
@@ -1,6 +1,9 @@
 package no.uib.inf273.operators
 
 import no.uib.inf273.Logger
+import no.uib.inf273.Main
+import no.uib.inf273.extra.filter
+import no.uib.inf273.extra.insert
 import no.uib.inf273.extra.randomizeExchange
 import no.uib.inf273.processor.Solution
 
@@ -107,6 +110,81 @@ interface Operator {
             return operateVesselTilFeasible(sol, vIndex, init, allowEqual) {
                 it.randomizeExchange()
             }
+        }
+
+        internal fun moveCargo(
+            sol: Solution,
+            sub: Array<IntArray>,
+            orgVesselIndex: Int,
+            destVesselIndex: Int,
+            cargoId: Int
+        ) {
+
+
+            val orgNew = removeCargo(sol, sub, orgVesselIndex, cargoId) ?: return
+            sub[orgVesselIndex] = orgNew
+
+            val destNew = addCargo(sol, sub, destVesselIndex, cargoId) ?: return
+            sub[destVesselIndex] = destNew
+
+
+            //reassemble the solution with the new vessel-cargo composition
+            sol.joinToArray(sub)
+        }
+
+
+        internal fun addCargo(sol: Solution, sub: Array<IntArray>, destVesselIndex: Int, cargoId: Int): IntArray? {
+
+            val destOldSize = sub[destVesselIndex].size
+            //the destination array needs to be two element larger for the new cargo to fit
+            val destNew = sub[destVesselIndex].copyOf(destOldSize + 2)
+            if (destOldSize > 0 && destVesselIndex != sub.size - 1) {
+                //insert the values randomly
+                destNew.insert(Main.rand.nextInt(destOldSize), cargoId)
+                //second time around we need to account for the element we just inserted
+                destNew.insert(Main.rand.nextInt(destOldSize + 1), cargoId)
+            } else {
+                //no need for randomness when placing cargo into an empty vessel or
+                // when placing it in the freight dummy vessel
+                destNew[destNew.size - 1] = cargoId
+                destNew[destNew.size - 2] = cargoId
+            }
+
+            val destFeasible = exchangeOnceTilFeasible(sol, destVesselIndex, destNew, true)
+            if (!destFeasible) {
+                ReinsertOnceOperator.log.trace { "Failed to add cargo $cargoId to vessel $destVesselIndex as no feasible arrangement could be found" }
+                return null
+            }
+
+            if (ReinsertOnceOperator.log.isDebugEnabled()) {
+                check(sol.isVesselFeasible(destVesselIndex, destNew)) {
+                    "Destination vessel $destVesselIndex not feasible"
+                }
+            }
+            return destNew
+        }
+
+        internal fun removeCargo(sol: Solution, sub: Array<IntArray>, orgVesselIndex: Int, cargoId: Int): IntArray? {
+            //create new array with two less elements as they will no longer be here
+            val orgNew = sub[orgVesselIndex].filter(cargoId, IntArray(sub[orgVesselIndex].size - 2))
+
+            //A vessel will always be feasible when removing a cargo.
+            // All it does in the worst case is force the vessel to wait longer at each port
+            check(sol.isVesselFeasible(orgVesselIndex, orgNew)) {
+                "Origin vessel $orgVesselIndex not feasible after removing a cargo"
+            }
+            return orgNew
+        }
+
+        //visible in a function for testing
+        internal fun getRandomVesselIndices(sub: Array<IntArray>): Pair<Int, Int> {
+            var orgVesselIndex: Int
+            var destVesselIndex: Int
+            do {
+                orgVesselIndex = Main.rand.nextInt(sub.size)
+                destVesselIndex = Main.rand.nextInt(sub.size)
+            } while (orgVesselIndex == destVesselIndex || sub[orgVesselIndex].isEmpty())
+            return orgVesselIndex to destVesselIndex
         }
     }
 }
--- src/main/kotlin/no/uib/inf273/operators/ReinsertOnceOperator.kt
+++ src/main/kotlin/no/uib/inf273/operators/ReinsertOnceOperator.kt
@@ -2,8 +2,8 @@ package no.uib.inf273.operators
 
 import no.uib.inf273.Logger
 import no.uib.inf273.Main
-import no.uib.inf273.extra.filter
-import no.uib.inf273.extra.insert
+import no.uib.inf273.operators.Operator.Companion.getRandomVesselIndices
+import no.uib.inf273.operators.Operator.Companion.moveCargo
 import no.uib.inf273.processor.Solution
 
 /**
@@ -18,69 +18,11 @@ object ReinsertOnceOperator : Operator {
         val sub = sol.splitToSubArray(true)
 
         //select two vessels where the origin vessel have cargoes
-        val (orgVesselIndex, destVesselIndex) = getVesselIndices(sub)
+        val (orgVesselIndex, destVesselIndex) = getRandomVesselIndices(sub)
 
         //pick a random cargo within the origin vessel
         val cargo = sub[orgVesselIndex].random(Main.rand)
 
-        //add both at the end of the new vessel. Do this first as the new destination vessel might not be feasible
-
-        val destOldSize = sub[destVesselIndex].size
-        //the destination array needs to be two element larger for the new cargo to fit
-        val destNew = sub[destVesselIndex].copyOf(destOldSize + 2)
-        if (destOldSize > 0 && destVesselIndex != sub.size - 1) {
-            //insert the values randomly
-            destNew.insert(Main.rand.nextInt(destOldSize), cargo)
-            //second time around we need to account for the element we just inserted
-            destNew.insert(Main.rand.nextInt(destOldSize + 1), cargo)
-        } else {
-            //no need for randomness when placing cargo into an empty vessel or
-            // when placing it in the freight dummy vessel
-            destNew[destNew.size - 1] = cargo
-            destNew[destNew.size - 2] = cargo
-        }
-        val destFeasible = Operator.exchangeOnceTilFeasible(sol, destVesselIndex, destNew, true)
-        if (!destFeasible) {
-            log.trace { "Failed to reinsert $cargo from vessel $orgVesselIndex to $destVesselIndex as no feasible arrangement could be found" }
-            return
-        }
-
-        if (log.isDebugEnabled()) {
-            check(sol.isVesselFeasible(destVesselIndex, destNew)) {
-                "Destination vessel $destVesselIndex not feasible"
-            }
-        }
-
-        sub[destVesselIndex] = destNew
-
-        //remove cargo from the original vessel
-
-        //create new array with two less elements as they will no longer be here
-        val orgNew = sub[orgVesselIndex].filter(cargo, IntArray(sub[orgVesselIndex].size - 2))
-
-        val orgFeasible = Operator.exchangeOnceTilFeasible(sol, orgVesselIndex, orgNew, true)
-        if (!orgFeasible) {
-            log.trace { "Failed to reinsert $cargo from vessel $orgVesselIndex to $orgVesselIndex as no feasible arrangement could be found" }
-            return
-        }
-        if (log.isDebugEnabled()) {
-            check(sol.isVesselFeasible(orgVesselIndex, orgNew)) {
-                "Origin vessel $orgVesselIndex not feasible"
-            }
-        }
-        sub[orgVesselIndex] = orgNew
-        sol.joinToArray(sub)
-
-    }
-
-    //visible in a function for testing
-    internal fun getVesselIndices(sub: Array<IntArray>): Pair<Int, Int> {
-        var orgVesselIndex: Int
-        var destVesselIndex: Int
-        do {
-            orgVesselIndex = Main.rand.nextInt(sub.size)
-            destVesselIndex = Main.rand.nextInt(sub.size)
-        } while (orgVesselIndex == destVesselIndex || sub[orgVesselIndex].isEmpty())
-        return orgVesselIndex to destVesselIndex
+        moveCargo(sol, sub, orgVesselIndex, destVesselIndex, cargo)
     }
 }
