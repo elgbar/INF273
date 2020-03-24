e008c66dd83aff50d01885870f1cd6c7d6789df5
Karl Henrik Elg <klink100@gmail.com>
2020-03-24 12:41:08 +0100

    Implement minimize wait time

--- src/main/kotlin/no/uib/inf273/operators/MinimizeWaitTime.kt
+++ src/main/kotlin/no/uib/inf273/operators/MinimizeWaitTime.kt
@@ -1,5 +1,6 @@
 package no.uib.inf273.operators
 
+import no.uib.inf273.extra.randomizeExchange
 import no.uib.inf273.processor.Solution
 
 /**
@@ -10,6 +11,47 @@ import no.uib.inf273.processor.Solution
 object MinimizeWaitTime : Operator() {
 
     override fun operate(sol: Solution) {
+        val subs = sol.splitToSubArray(true)
 
+        //find the vessel with the greatest waiting time
+        // note that we do not select the vessel with the greatest average waiting time just the global max waiting time
+        val vesselMeta = subs.mapIndexed() { index, sub ->
+            sol.generateVesselRouteMetadata(index, sub)
+        }.maxBy {
+            it.portTardiness.max() ?: -1
+        } ?: error("Failed to find a vessel")
+
+
+        val maxTardiness = vesselMeta.portTardiness.max()
+        if (maxTardiness == null) {
+            UrgentFirst.log.debug { "No cargoes have " }
+            return
+        }
+
+        val vIndex = vesselMeta.vesselIndex
+
+        //find what cargo is waiting the longest
+        val sub = subs[vIndex]
+
+        val maxTries = 20
+        var tryNr = 0
+
+        val solCopy = sol.copy()
+
+        //then try and minimize the waiting time
+        do {
+            if (operateVesselTilFeasible(solCopy, vIndex, sub) {
+                    it.randomizeExchange()
+                }) {
+                val newMeta = solCopy.generateVesselRouteMetadata(vIndex, sub)
+                val newMaxTardiness = newMeta.portTardiness.max()
+                    ?: error("Failed to calculate max new tardiness for ${newMeta.portTardiness}")
+
+                if (newMaxTardiness < maxTardiness) {
+                    UrgentFirst.log.debug { "New smaller maximum tardiness found! new = $newMaxTardiness, old = $maxTardiness" }
+                    break
+                }
+            }
+        } while (tryNr++ < maxTries)
     }
 }
