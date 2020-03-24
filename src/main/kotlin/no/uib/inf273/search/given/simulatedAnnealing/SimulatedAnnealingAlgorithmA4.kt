31d1ff1c9b0647b7bb332ee5289ff3871c5bed56
Karl Henrik Elg <klink100@gmail.com>
2020-03-24 13:14:13 +0100

    Tune SimulatedAnnealingAlgorithmA4
    Decrease ReinsertOnceOperator

--- src/main/kotlin/no/uib/inf273/search/given/simulatedAnnealing/SimulatedAnnealingAlgorithmA4.kt
+++ src/main/kotlin/no/uib/inf273/search/given/simulatedAnnealing/SimulatedAnnealingAlgorithmA4.kt
@@ -5,7 +5,7 @@ import no.uib.inf273.operators.MinimizeWaitTime
 import no.uib.inf273.operators.given.ReinsertOnceOperator
 
 object SimulatedAnnealingAlgorithmA4 : SimulatedAnnealingAlgorithm(
-    0.50 to MinimizeWaitTime,
-    0.70 to MinimizeFreight,
-    fallbackOp = ReinsertOnceOperator(1.0)
+    0.30 to MinimizeWaitTime,
+    0.50 to MinimizeFreight,
+    fallbackOp = ReinsertOnceOperator(0.7)
 ) {}
