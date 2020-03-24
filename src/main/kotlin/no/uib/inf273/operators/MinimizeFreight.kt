5763c1a6dd46b0b99c54f9b171e5f01fcf06e607
Karl Henrik Elg <klink100@gmail.com>
2020-03-24 12:25:08 +0100

    Make minimize freight select the best cargo to move out of the dummy

--- src/main/kotlin/no/uib/inf273/operators/MinimizeFreight.kt
+++ src/main/kotlin/no/uib/inf273/operators/MinimizeFreight.kt
@@ -10,21 +10,47 @@ import no.uib.inf273.processor.Solution
  * If we minimize number of cargoes we do not transport the cost will (hopefully) go down
  */
 object MinimizeFreight : Operator() {
-
+    
     override fun operate(sol: Solution) {
         val subs = sol.splitToSubArray(true)
         val freights = subs.last()
         if (freights.isEmpty()) return //freight is empty nothing we can do
 
-        val cargo = freights.random(Main.rand) //find a random cargo to transport
-
-        //subtract one from the available vessels to not select the freight array
-        val destIndex = Main.rand.nextInt(subs.size - 1)
         val orgIndex = subs.size - 1 //origin is always dummy vessel
 
-        //move the cargo to the new vessel
-        moveCargo(sol, subs, orgIndex, destIndex, cargo) {
-            it.randomizeExchange()
+        val orgSubCpy = subs[orgIndex].clone()
+
+        //sort the cargoes by how much they cost of not transporting
+        val sortedCargoes = freights.toHashSet().sortedByDescending { sol.data.cargoFromId(it).ntCost }
+        log.debug { "Can move ${sortedCargoes.size} cargoes from dummy!" }
+
+        outer@
+        for (cargo in sortedCargoes) {
+
+            //is there anything we can sort this list by?
+            // maybe use the arches somehow, we have the origin at least but not the destination (unless we say that we take it there directly)
+            val validDestVessels = sol.data.vessels.filter { it.canTakeCargo(cargo) }.shuffled(Main.rand)
+            for (vessel in validDestVessels) {
+                val destIndex = vessel.index()
+
+                //keep a copy of the destination in case we couldn't move the cargo there for some reason
+                val destSubCpy = subs[destIndex].clone()
+
+                //move the cargo to the new vessel
+                val moved = moveCargo(sol, subs, orgIndex, destIndex, cargo) {
+                    it.randomizeExchange()
+                }
+
+                if (moved) {
+                    log.debug { "Cargo ${sol.data.cargoFromId(cargo)} can be moved from freight to dest $vessel" }
+                    break@outer
+                } else {
+                    //we didn't move so the subarray needs to be restored
+                    subs[orgIndex] = orgSubCpy.clone()
+                    //no need to clone the destination as that will change for each iteration
+                    subs[destIndex] = destSubCpy
+                }
+            }
         }
     }
 }
