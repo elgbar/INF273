66d7af045a4078b0215d66d7f37d74b90b265824
Karl Henrik Elg <klink100@gmail.com>
2020-03-29 15:48:52 +0200

    Select better vessel/cargoes

--- src/main/kotlin/no/uib/inf273/operators/MinimizeWaitTime.kt
+++ src/main/kotlin/no/uib/inf273/operators/MinimizeWaitTime.kt
@@ -4,7 +4,11 @@ import no.uib.inf273.extra.randomizeExchange
 import no.uib.inf273.processor.Solution
 
 /**
- * Minimize the cargo that is waiting the longest globally.This operators find the cargo which is currently waiting for port opening the longest and to and change the vessel cargo order in such a way the the maximum wait time is lower.
+ * Minimize the cargo that is waiting the longest globally.
+ * This operators find the cargo which is currently waiting for port opening the longest and to and change the vessel cargo
+ * order in such a way the the maximum wait time is lower.
+ *
+ * This operator helps with intensification as it optimizes a vessels cargo route.
  *
  * @author Elg
  */
@@ -16,16 +20,21 @@ object MinimizeWaitTime : Operator() {
         //find the vessel with the greatest waiting time
         // note that we do not select the vessel with the greatest average waiting time just the global max waiting time
         val vesselMeta = subs.mapIndexed() { index, sub ->
-            sol.generateVesselRouteMetadata(index, sub)
-        }.maxBy {
+            sol.generateVesselRouteMetadata(
+                index,
+                sub
+            ) // generate all info we know about this vessel (objval, port tardiness, etc)
+        }.filter { !sol.data.isDummyVessel(it.vesselIndex) && it.arr.size > 2 }.maxBy {
             it.portTardiness.max() ?: -1
-        } ?: error("Failed to find a vessel")
+        }
 
-        var maxTardiness = vesselMeta.portTardiness.max()
-        if (maxTardiness == null) {
-            log.debug { "No cargoes to optimize" }
+        if (vesselMeta == null) {
+            //Nothing to do, vessel is either the dummy vessel or has zero or one cargo
+            log.debug { "Failed to find a vessel to change" }
             return
         }
+
+        val maxTardiness = vesselMeta.portTardiness.max() ?: error("Failed to find a cargo in vessel $vesselMeta")
 
         val vIndex = vesselMeta.vesselIndex
 
@@ -39,10 +48,7 @@ object MinimizeWaitTime : Operator() {
 
         //then try and minimize the waiting time
         do {
-            if (operateVesselTilFeasible(solCopy, vIndex, sub) {
-                    
-                    it.randomizeExchange()
-                }) {
+            if (operateVesselTilFeasible(solCopy, vIndex, sub) { it.randomizeExchange() }) {
                 val newMeta = solCopy.generateVesselRouteMetadata(vIndex, sub)
                 val newMaxTardiness = newMeta.portTardiness.max()
                     ?: error("Failed to calculate max new tardiness for ${newMeta.portTardiness}")
--- src/main/kotlin/no/uib/inf273/operators/given/ReinsertOnceOperator.kt
+++ src/main/kotlin/no/uib/inf273/operators/given/ReinsertOnceOperator.kt
@@ -1,7 +1,6 @@
 package no.uib.inf273.operators.given
 
 import no.uib.inf273.Main
-import no.uib.inf273.extra.randomizeExchange
 import no.uib.inf273.operators.Operator
 import no.uib.inf273.processor.Solution
 
@@ -17,15 +16,20 @@ class ReinsertOnceOperator(private val discourageFreightPercent: Double = 0.0) :
     override fun operate(sol: Solution) {
         val sub = sol.splitToSubArray(true)
 
-        //select two vessels where the origin vessel have cargoes
-        val (orgVesselIndex, destVesselIndex) = selectTwoRandomVessels(sub, discourageFreightPercent)
+        var cargo: Int
+        var indices: Pair<Int, Int>
+        do {
+            //select two vessels where the origin vessel have cargoes
+            indices = selectTwoRandomVessels(sub, discourageFreightPercent)
 
-        //pick a random cargo within the origin vessel
-        val cargo = sub[orgVesselIndex].random(Main.rand)
+            //pick a random cargo within the origin vessel
+            cargo = sub[indices.first].random(Main.rand)
+        } while (!sol.data.canVesselTakeCargo(indices.second, cargo))
 
-        if (moveCargo(sol, sub, orgVesselIndex, destVesselIndex, cargo) {
-                it.randomizeExchange()
-            }) {
+        val orgVesselIndex = indices.first
+        val destVesselIndex = indices.second
+
+        if (moveCargo(sol, sub, orgVesselIndex, destVesselIndex, cargo)) {
             log.debug {
                 "Cargo ${sol.data.cargoFromId(cargo)} can be moved from $orgVesselIndex " +
                         "to dest $destVesselIndex"
